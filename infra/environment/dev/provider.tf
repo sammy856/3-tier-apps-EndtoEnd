4bade18557cec1c083201ee682ae69f0f8f04b5f
sammy856 <61598482+sammy856@users.noreply.github.com>
2026-07-02 16:44:08 +0530

    Configure Terraform provider and backend for Azure

--- infra/environment/dev/provider.tf
+++ infra/environment/dev/provider.tf
@@ -1,13 +1,32 @@
 terraform {
+
   required_providers {
+
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>4.1"
     }
+
   }
+
+  backend "azurerm" {
+
+    resource_group_name  = "rg-tfstate"
+
+    storage_account_name = "stgdemo2026tf2"
+
+    container_name       = "mycontainer"
+
+    key                  = "dev.terraform.tfstate"
+
+  }
+
 }
 
 provider "azurerm" {
+
   features {}
+
   subscription_id = "8432b329-2a25-4ccb-9771-c72ad6d864df"
+
 }
